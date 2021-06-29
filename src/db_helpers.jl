5e906a36b297e5c3188fc3ae8815056ae418873f
JokingHero <kornel.labun@gmail.com>
2021-06-29 18:27:55 +0200

    increasing speed through broadcasting

--- src/db_helpers.jl
+++ src/db_helpers.jl
@@ -72,44 +72,43 @@ function gatherofftargets(
 
     if length(query) != 0
         chrom_max = lastindex(chrom)
-        guides = Base.map(findall(query, chrom)) do x
-            guide = LongDNASeq(chrom[x])
-            guide = removepam(guide, pam_loci)
-            # add extension
-            if dbi.motif.extends5 && reverse_comp
-                # CCN ... EXT
-                guide = guide * getExt3(chrom, chrom_max, last(x) + 1, dbi.motif.distance)
-                guide = complement(guide)
-                pos_ = first(x)
-                # becomes GGN ... EXT
-            elseif dbi.motif.extends5 && !reverse_comp 
-                # EXT ... NGG
-                guide = getExt5(chrom, first(x) - 1, dbi.motif.distance) * guide
-                guide = reverse(guide)
-                pos_ = last(x)
-                # becomes GGN ... EXT
-            elseif !dbi.motif.extends5 && reverse_comp 
-                # EXT ... NAA
-                guide = getExt5(chrom, first(x) - 1, dbi.motif.distance) * guide
-                guide = reverse_complement(guide)
-                pos_ = last(x)
-                # becomes TTA ... EXT
-            else #!dbi.motif.extends5 && !reverse_comp
-                # TTN ... EXT
-                guide = guide * getExt3(chrom, chrom_max, last(x) + 1, dbi.motif.distance)
-                pos_ = first(x)
-            end
-            pos_ = convert(dbi.pos_type, pos_)
-            loc = Loc(chrom_name_, pos_, !reverse_comp)
+        guides_pos = findall(query, chrom)
+        guides = Base.map(x -> removepam(chrom[x], pam_loci), guides_pos)
 
-            gprefix = guide[1:prefix_len]
-            gsuffix = guide[prefix_len+1:end]
-            return (gprefix, gsuffix, loc)
+        # add extension
+        if dbi.motif.extends5 && reverse_comp
+            # CCN ... EXT
+            ext = last.(guides_pos) .+ 1
+            ext = Base.map(x -> getExt3(chrom, chrom_max, x, dbi.motif.distance), ext)
+            guides .= complement.(guides .* ext)
+            guides_pos = first.(guides_pos)
+            # becomes GGN ... EXT
+        elseif dbi.motif.extends5 && !reverse_comp 
+            # EXT ... NGG
+            ext = first.(guides_pos) .- 1
+            ext = Base.map(x -> getExt5(chrom, x, dbi.motif.distance), ext)
+            guides .= reverse.(ext .* guides)
+            guides_pos = last.(guides_pos)
+            # becomes GGN ... EXT
+        elseif !dbi.motif.extends5 && reverse_comp 
+            # EXT ... NAA
+            ext = first.(guides_pos) .- 1
+            ext = Base.map(x -> getExt5(chrom, x, dbi.motif.distance), ext)
+            guides .= reverse_complement.(ext .* guides)
+            guides_pos = last.(guides_pos)
+            # becomes TTA ... EXT
+        else #!dbi.motif.extends5 && !reverse_comp
+            # TTN ... EXT
+            ext = last.(guides_pos) .+ 1
+            ext = Base.map(x -> getExt3(chrom, chrom_max, x, dbi.motif.distance), ext)
+            guides = guides .* ext
+            guides_pos = first.(guides_pos)
         end
-        
-        gprefix = getindex.(guides, 1)
-        gsuffix = getindex.(guides, 2)
-        loc = getindex.(guides, 3)
+        guides_pos = convert.(dbi.pos_type, guides_pos)
+        loc = Loc.(chrom_name_, guides_pos, !reverse_comp)
+
+        gprefix = getindex.(guides, [1:prefix_len])
+        gsuffix = getindex.(guides, [prefix_len+1:length(guides[1])])
         uprefixes = unique(gprefix)
 
         output = ThreadsX.map(uprefixes) do prefix
